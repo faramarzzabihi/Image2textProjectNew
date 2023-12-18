425648c9011dc215060d8163305bcafc962a4fce
faramarz.zabihi@yahoo.com <faramarz.zabihi@yahoo.com>
2023-12-18 21:39:14 +0330

    add padel ocer

--- image_converter/Utils/PadellOCR.py
+++ image_converter/Utils/PadellOCR.py
@@ -5,13 +5,13 @@ from PIL import Image
 def PaddleOCRT2I(img,LineSize):
     
   ocr = PaddleOCR(lang='fa')
-  result = ocr.ocr(img)
+  result = ocr.ocr(np.array(img))
 
-  #LineSize=10
+  yLineSize=LineSize
   text=""
   line_text=""
   for line in result:
-    if abs(line[0][0][1]-LineSize) <10:
+    if abs(line[0][0][1]-yLineSize) <LineSize:
       line_text=line[1][0]+' '+line_text
     else:
       text+=line_text
