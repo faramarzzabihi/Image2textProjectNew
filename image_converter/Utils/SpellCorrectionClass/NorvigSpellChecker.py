e81d6b2d2aca1a3804d2218f0473e7d5683cc706
faramarz.zabihi@yahoo.com <faramarz.zabihi@yahoo.com>
2023-12-21 09:25:34 +0330

    some cahnge

--- image_converter/Utils/SpellCorrectionClass/NorvigSpellChecker.py
+++ image_converter/Utils/SpellCorrectionClass/NorvigSpellChecker.py
@@ -8,19 +8,15 @@ class NorvigSpellChecker:
         self.letters = 'آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی'
 
     def P(self, word):
-        "Probability of word."
         return self.WORDS[word] / self.N
 
     def correction(self, word):
-        "Most probable spelling correction for word."
         return max(self.word_pool(word), key=self.P)
 
     def word_pool(self, word):
-        "Generate a pool of possible words."
         return (self.known([word]) or self.known(self.edits1(word)) or self.known(self.edits2(word)) or [word])
 
     def candidates(self, word):
-        "Generate possible spelling corrections for word."
         if self.WORDS[word] < 30 or len(word) == 1 and word not in hazm.stopwords_list():
             candidates_list = list(self.known(self.edits1(word)))
             candidates_list.sort(key=self.P, reverse=True)
@@ -29,11 +25,9 @@ class NorvigSpellChecker:
             return word
 
     def known(self, words):
-        "The subset of words that appear in the dictionary of WORDS."
         return set(w for w in words if w in self.WORDS)
 
     def edits1(self, word):
-        "All edits that are one edit away from word."
         splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
         deletes = [L + R[1:] for L, R in splits if R]
         transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
@@ -42,6 +36,5 @@ class NorvigSpellChecker:
         return set(deletes + transposes + replaces + inserts)
 
     def edits2(self, word):
-        "All edits that are two edits away from word."
         return (e2 for e1 in self.edits1(word) for e2 in self.edits1(e1))
     
